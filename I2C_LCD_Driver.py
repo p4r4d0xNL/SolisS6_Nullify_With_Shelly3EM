06b25a879fdf1dc05ac08604cc3894a5e456a2e2
Chris Staring <50363940+p4r4d0xNL@users.noreply.github.com>
2023-12-03 20:02:31 +0100

    Update I2C_LCD_Driver.py

--- I2C_LCD_Driver.py
+++ I2C_LCD_Driver.py
@@ -19,39 +19,35 @@ class i2c_device:
       self.addr = addr
       self.bus = smbus.SMBus(port)
 
-# Write a single command
+# Schreibe eine Zeile
    def write_cmd(self, cmd):
       self.bus.write_byte(self.addr, cmd)
       sleep(0.0001)
 
-# Write a command and argument
+# Schreibe einen Befehl in einer Zeile
    def write_cmd_arg(self, cmd, data):
       self.bus.write_byte_data(self.addr, cmd, data)
       sleep(0.0001)
 
-# Write a block of data
+# Schreibe einen Block
    def write_block_data(self, cmd, data):
       self.bus.write_block_data(self.addr, cmd, data)
       sleep(0.0001)
 
-# Read a single byte
+# Lese einen Byte
    def read(self):
       return self.bus.read_byte(self.addr)
 
-# Read
+# Lesen
    def read_data(self, cmd):
       return self.bus.read_byte_data(self.addr, cmd)
 
-# Read a block of data
+# Lese einen Block an Daten
    def read_block_data(self, cmd):
       return self.bus.read_block_data(self.addr, cmd)
 
-
-
-# LCD Address
+# Befehle
 ADDRESS = 0x27
-
-# commands
 LCD_CLEARDISPLAY = 0x01
 LCD_RETURNHOME = 0x02
 LCD_ENTRYMODESET = 0x04
@@ -60,45 +56,35 @@ LCD_CURSORSHIFT = 0x10
 LCD_FUNCTIONSET = 0x20
 LCD_SETCGRAMADDR = 0x40
 LCD_SETDDRAMADDR = 0x80
-
-# flags for display entry mode
 LCD_ENTRYRIGHT = 0x00
 LCD_ENTRYLEFT = 0x02
 LCD_ENTRYSHIFTINCREMENT = 0x01
 LCD_ENTRYSHIFTDECREMENT = 0x00
-
-# flags for display on/off control
 LCD_DISPLAYON = 0x04
 LCD_DISPLAYOFF = 0x00
 LCD_CURSORON = 0x02
 LCD_CURSOROFF = 0x00
 LCD_BLINKON = 0x01
 LCD_BLINKOFF = 0x00
-
-# flags for display/cursor shift
 LCD_DISPLAYMOVE = 0x08
 LCD_CURSORMOVE = 0x00
 LCD_MOVERIGHT = 0x04
 LCD_MOVELEFT = 0x00
-
-# flags for function set
 LCD_8BITMODE = 0x10
 LCD_4BITMODE = 0x00
 LCD_2LINE = 0x08
 LCD_1LINE = 0x00
 LCD_5x10DOTS = 0x04
 LCD_5x8DOTS = 0x00
-
-# flags for backlight control
 LCD_BACKLIGHT = 0x08
 LCD_NOBACKLIGHT = 0x00
 
-En = 0b00000100 # Enable bit
-Rw = 0b00000010 # Read/Write bit
-Rs = 0b00000001 # Register select bit
+En = 0b00000100 # Enable Bit
+Rw = 0b00000010 # Lese/Schreibe Bit
+Rs = 0b00000001 # Registerauswahl Bit
 
 class lcd:
-   #initializes objects and lcd
+   # Initialisidert das LCD
    def __init__(self):
       self.lcd_device = i2c_device(ADDRESS)
 
@@ -113,8 +99,6 @@ class lcd:
       self.lcd_write(LCD_ENTRYMODESET | LCD_ENTRYLEFT)
       sleep(0.2)
 
-
-   # clocks EN to latch command
    def lcd_strobe(self, data):
       self.lcd_device.write_cmd(data | En | LCD_BACKLIGHT)
       sleep(.0005)
@@ -125,19 +109,14 @@ class lcd:
       self.lcd_device.write_cmd(data | LCD_BACKLIGHT)
       self.lcd_strobe(data)
 
-   # write a command to lcd
    def lcd_write(self, cmd, mode=0):
       self.lcd_write_four_bits(mode | (cmd & 0xF0))
       self.lcd_write_four_bits(mode | ((cmd << 4) & 0xF0))
 
-   # write a character to lcd (or character rom) 0x09: backlight | RS=DR<
-   # works!
    def lcd_write_char(self, charvalue, mode=1):
       self.lcd_write_four_bits(mode | (charvalue & 0xF0))
       self.lcd_write_four_bits(mode | ((charvalue << 4) & 0xF0))
   
-
-   # put string function
    def lcd_display_string(self, string, line):
       if line == 1:
          self.lcd_write(0x80)
@@ -151,26 +130,22 @@ class lcd:
       for char in string:
          self.lcd_write(ord(char), Rs)
 
-   # clear lcd and set to home
    def lcd_clear(self):
       self.lcd_write(LCD_CLEARDISPLAY)
       self.lcd_write(LCD_RETURNHOME)
 
-   # define backlight on/off (lcd.backlight(1); off= lcd.backlight(0)
-   def backlight(self, state): # for state, 1 = on, 0 = off
+   def backlight(self, state):
       if state == 1:
          self.lcd_device.write_cmd(LCD_BACKLIGHT)
       elif state == 0:
          self.lcd_device.write_cmd(LCD_NOBACKLIGHT)
 
-   # add custom characters (0 - 7)
    def lcd_load_custom_chars(self, fontdata):
       self.lcd_write(0x40);
       for char in fontdata:
          for line in char:
             self.lcd_write_char(line)         
-         
-   # define precise positioning (addition from the forum)
+            
    def lcd_display_string_pos(self, string, line, pos):
     if line == 1:
       pos_new = pos
